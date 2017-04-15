e292caa3939911920801f65144aa6f3bd14d0230
Christopher Hair <chair@mystikweb.net>
2017-04-15 10:59:59 -0400

    Question 4 completed and starting on Question 5

--- GettingAndCleaningData/Week2/Question4.R
+++ GettingAndCleaningData/Week2/Question4.R
@@ -2,3 +2,4 @@ contact_page <- url("http://biostat.jhsph.edu/~jleek/contact.html")
 contact_content <- readLines(contact_page, n = 100)
 close(contact_page)
 
+char_count <- sapply(contact_content[c(10,20,30,100)], nchar)
